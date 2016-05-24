dc0de3cabda560d02f2087c9606eca0e2f2ccd53
Filip Bartodziej <filipbartodziej@gmail.com>
2016-05-24 20:01:52 +0200

    Added scroll bar to the game.

--- src/games/BasicGame.java
+++ src/games/BasicGame.java
@@ -1,19 +1,28 @@
 package games;
 
 import games.Game;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.scene.control.Button;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -26,6 +35,7 @@ public class BasicGame implements Game {
     public BasicGame() {}
 
     public void play(int wordsQuantity) {
+
         Map<String, String> gameContent = new HashMap<>();
         Stage window = new Stage();
         window.setTitle("Basic Game");
@@ -50,13 +60,18 @@ public class BasicGame implements Game {
         }
         //
 
-        // Submit
+        // Submit button
         Button submit = new Button("Submit");
         submit.setOnAction(event -> {
             int result=0;
             for (GamePair gp : matchings) {
-                if (testComponets.get(gp.label.getText()).equals(gp.textField.getText()))
+                if (testComponets.get(gp.label.getText()).equals(gp.textField.getText())) {
                     result++;
+                    gp.textField.setStyle("-fx-text-fill: green");
+                }
+                else
+                    gp.textField.setStyle("-fx-text-fill: red");
+
             }
             grid.getChildren().remove(submit);
 
@@ -64,13 +79,19 @@ public class BasicGame implements Game {
             Label labelOfResult = new Label();
             labelOfResult.setText(""+result+"/"+wordsQuantity);
             labelOfResult.setTextFill(Color.RED);
+            labelOfResult.setStyle("-fx-text-fill: lightseagreen; -fx-font-size: 20;");
 
             // Result icon
-            //ImageView resultIcon = new ImageView(new Image(BasicGame.class.getResourceAsStream("ok.png")));
+            double grade = (double)result / (double)wordsQuantity;
+            ImageView resultIcon;
+            if (grade>0.75)
+                resultIcon = new ImageView(new Image(BasicGame.class.getResourceAsStream("smallok.png")));
+            else
+                resultIcon = new ImageView(new Image(BasicGame.class.getResourceAsStream("smallnotok.png")));
 
             // Result HBox
             HBox resultHBox= new HBox();
-            resultHBox.getChildren().addAll(labelOfResult);
+            resultHBox.getChildren().addAll(labelOfResult, resultIcon);
             resultHBox.setSpacing(20);
             resultHBox.setAlignment(Pos.CENTER);
             GridPane.setHalignment(resultHBox, HPos.CENTER);
@@ -89,8 +110,32 @@ public class BasicGame implements Game {
         Button quit = new Button("Quit");
         quit.setOnAction(event -> window.close());
 
+        // Group
+        Group root = new Group();
 
-        window.setScene(new Scene(grid));
+        // Scene
+        Scene scene = new Scene(root, 250, 300);
+
+        // Scroll Bar
+        ScrollBar sb = new ScrollBar();
+        sb.setLayoutX(scene.getWidth()-sb.getWidth());
+        sb.setMin(0);
+        sb.setMax(30 * wordsQuantity);
+        sb.setPrefHeight(300);
+        sb.setOrientation(Orientation.VERTICAL);
+
+
+        sb.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov,
+                                Number old_val, Number new_val) {
+                grid.setLayoutY(-new_val.doubleValue());
+            }
+        });
+        grid.getChildren().add(sb);
+
+        root.getChildren().addAll(grid, sb);
+
+        window.setScene(scene);
         window.showAndWait();
     }
 
