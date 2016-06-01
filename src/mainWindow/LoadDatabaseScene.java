86efd28d58f41b49dc347df9e192f9ad5a12d081
Paweł Bąk <baczus0@gmail.com>
2016-06-01 03:40:00 +0200

    - handling exception when bad file - better file imput - small cosmetic changes

--- src/mainWindow/LoadDatabaseScene.java
+++ src/mainWindow/LoadDatabaseScene.java
@@ -2,6 +2,7 @@ package mainWindow;
 
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -33,14 +34,20 @@ public class LoadDatabaseScene {
         infoLabel0.setAlignment(Pos.CENTER);
         infoLabel0.setMaxWidth(180);
 
-        Label infoLabel1=new Label("Set your database name:");
+        Label infoLabel1=new Label("Database name:");
         infoLabel1.setAlignment(Pos.CENTER);
         infoLabel1.setMaxWidth(180);
 
+
         TextField databaseName=new TextField();
-        databaseName.setPromptText("Set your database name");
+        databaseName.setPromptText("Set your database name:");
         databaseName.setAlignment(Pos.CENTER);
         databaseName.setMaxWidth(180);
+        if(selectedFile == null) databaseName.setDisable(true);
+        else databaseName.setDisable(false);
+
+
+
 
         Button backButton=new Button("Back");
         backButton.setOnAction(e-> {
@@ -55,18 +62,25 @@ public class LoadDatabaseScene {
             if (selectedFile != null) {
                 System.out.println(databaseName.getText());
                 try {
+                    if(!selectedFile.getName().endsWith(".txt"))
+                        throw new NotTxtExtensionException();
+
                     Files.copy(Paths.get(selectedFile.getAbsolutePath()),
                             Paths.get("src"+ File.separator+"databases"+ File.separator + databaseName.getText() + ".txt"), StandardCopyOption.REPLACE_EXISTING);
-                } catch (IOException e1) {
-                    e1.printStackTrace();
-                }
-                mainDatabase.insert(databaseName.getText(),"src"+ File.separator+"databases"+ File.separator + databaseName.getText() + ".txt");
-                try {
+
+                    mainDatabase.insert(databaseName.getText(),"src"+ File.separator+"databases"+ File.separator + databaseName.getText() + ".txt");
+
                     if(!Files.exists(Paths.get("src"+ File.separator+"databases"+ File.separator+"importedDatabases.txt")))
                         Files.createFile(Paths.get("src"+ File.separator+"databases"+ File.separator+"importedDatabases.txt"));
                     Files.write(Paths.get("src"+ File.separator+"databases"+ File.separator+"importedDatabases.txt"), (databaseName.getText()+System.lineSeparator()).getBytes(), StandardOpenOption.APPEND);
-                }catch (IOException e2) {
-                    e2.printStackTrace();
+
+                    stackScene.pop();
+                    window.setScene(stackScene.peek());
+
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                } catch (InvalidPathException | NotTxtExtensionException e1) {
+                    showAlertBadPath();
                 }
             }
         });
@@ -75,6 +89,8 @@ public class LoadDatabaseScene {
         Button loadDatabaseButton=new Button("Choose database file");
         loadDatabaseButton.setOnAction(e-> {
             FileChooser fileChooser=new FileChooser();
+            fileChooser.setInitialDirectory( // set default directory to databases
+                    new File("src" + System.getProperty("file.separator") + "databases"));
             fileChooser.setTitle("Choose database file");
             selectedFile=fileChooser.showOpenDialog(window);
             if (selectedFile!=null){
@@ -100,8 +116,18 @@ public class LoadDatabaseScene {
         loadDatabaseScene=new Scene(loadDatabaseLayout,400,400);
     }
 
+    private void showAlertBadPath() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Błąd odczytu");
+        alert.setHeaderText("Błąd odczutu pliku");
+        alert.setContentText("Błędna ścieżka lub plik nie jest bazą danych!");
+        alert.showAndWait();
+    }
+
     Scene getScene(){
         return loadDatabaseScene;
     }
+
+    private class NotTxtExtensionException extends Exception {}
 
 }
